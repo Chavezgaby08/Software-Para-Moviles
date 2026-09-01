55892244ae5f2b6b1a9483fcb8cc921438837e4e
Chavezgaby08 <gabriela.chavez@uao.edu.co>
2026-09-01 14:36:49 -0500

    Delete Challenge01 directory

--- Challenge01/src/Components/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loader: React.FC = () => {
-  return <h3>Cargando contactos...</h3>;
-};
-
-export default Loader;
--- Challenge01/src/Types/contact.ts
+++
@@ -1,5 +0,0 @@
-export interface Contact {
-  id: number;
-  nombre: string;
-  telefono: string;
-}
