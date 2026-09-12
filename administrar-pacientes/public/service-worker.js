ab5bc8e4228d662abb0bbef3bbbb7a9cb8b75a8d
Gabriela <gabriela.chavez@uao.edu.co>
2026-09-12 16:37:06 -0500

    Correcciones del service worker

--- administrar-pacientes/public/service-worker.js
+++ administrar-pacientes/public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mediclinic-pwa-v1';
+const CACHE_NAME = 'mediclinic-pwa-v2';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -31,9 +31,21 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok) {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((response) => response || caches.match('/index.html')))
   );
 });
